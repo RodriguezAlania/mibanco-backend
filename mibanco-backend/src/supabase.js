012f049e5a9d94d26317d5109ebb0a2621941f24
RodriguezAlania <60878866@continental.edu.pe>
2026-06-26 15:39:48 -0500

    Conectar a Neon y fix CORS

--- mibanco-backend/src/supabase.js
+++ mibanco-backend/src/supabase.js
@@ -1,12 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'bd_core_financiero_MiBanco',
-  user: 'postgres',
-  password: '12345678',
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
-
